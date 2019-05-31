9d3e8d24886101752f613b270bfc4e03b33e06c7
mostlyjason <jason.skowronski@gmail.com>
2019-05-31 16:56:52 -0700

    Updating code to support new decision tree model

--- app.py
+++ app.py
@@ -4,20 +4,14 @@ from flask import Flask
 from flask_restful import reqparse, Api, Resource
 import pickle
 from model import RFModel
-import pandas as pd
-import json
-import datetime as dt
-import difflib
-from textblob import TextBlob
 import sys
-import numpy as np
 
 app = Flask(__name__)
 api = Api(app)
 
 model = RFModel()
 
-clf_path = 'lib/models/RandomForestClassifier.pkl'
+clf_path = 'lib/models/DecisionTreeClassifier.pkl'
 with open(clf_path, 'rb') as f:
     model.clf = pickle.load(f)
 
@@ -76,84 +70,12 @@ class Botidentification(Resource):
         # Use parser and find the user's query
         args = parser.parse_args()
         
-        no_follow = bool(args['no_follow'])
-        created_utc = int(args['created_utc'])
-        author_verified = bool(args['author_verified'])
-        author_comment_karma = float(args['author_comment_karma'])
-        author_link_karma = float(args['author_link_karma'])
-        over_18 = bool(args['over_18'])
-        is_submitter = bool(args['is_submitter'])
-        body = str(args['body'])
-        recent_comments = str(args['recent_comments'])
-        recent_num_comments = 0
-        recent_num_last_30_days = 0
-        recent_avg_no_follow = 0
-        recent_avg_gilded = 0
-        recent_avg_responses = 0
-        recent_percent_neg_score = 0
-        recent_avg_score = 0
-        recent_min_score = 0
-        recent_avg_controversiality = 0
-        recent_avg_ups = 0
-        recent_avg_diff_ratio = 0
-        recent_max_diff_ratio = 0
-        recent_avg_sentiment_polarity = 0
-        recent_min_sentiment_polarity = 0
+        print("recent:", args['recent_comments'])
         
-        created_utc = pd.to_datetime(created_utc, unit='s')
+        clean_data = model.clean_data(args)
+        print(clean_data)
         
-        def diff_ratio(_a, _b):
-            return difflib.SequenceMatcher(a=_a,b=_b).ratio()
-
-        def last_30(a, b):
-            return a - dt.timedelta(days=30) < pd.to_datetime(b, unit='s')
-        
-        recent_comments = pd.read_json(recent_comments, dtype={
-            "banned_by": str,
-            "no_follow": bool,
-            "link_id": str,
-            "gilded": np.float64,
-            "author": str,
-            "author_verified": bool,
-            "author_comment_karma": np.float64,
-            "author_link_karma": np.float64,
-            "num_comments": np.float64,
-            "created_utc": np.float64,
-            "score": np.float64,
-            "over_18": bool,
-            "body": str,
-            "downs": np.float64,
-            "is_submitter": bool,
-            "num_reports": np.float64,
-            "controversiality": np.float64,
-            "quarantine": bool,
-            "ups": np.float64})
-        if(len(recent_comments) > 0):
-            recent_num_comments = len(recent_comments)
-            recent_num_last_30_days = recent_comments['created_utc'].apply(lambda x: last_30(created_utc, x)).sum()
-            recent_avg_no_follow = recent_comments['no_follow'].mean()
-            recent_avg_gilded = recent_comments['gilded'].mean()
-            recent_avg_responses = recent_comments['num_comments'].mean()
-            recent_percent_neg_score = recent_comments['score'].apply(lambda x: x < 0).mean() * 100
-            recent_avg_score = recent_comments['score'].mean()
-            recent_min_score = recent_comments['score'].min()
-            recent_avg_controversiality = recent_comments['controversiality'].mean()
-            recent_avg_ups = recent_comments['ups'].mean()
-            diff = recent_comments['body'].str.slice(stop=200).fillna('').apply(lambda x: diff_ratio(body, x))
-            recent_avg_diff_ratio = diff.mean()
-            recent_max_diff_ratio = diff.max()
-            scores = recent_comments['body'].append(pd.Series([body])).apply(lambda x: TextBlob(x).sentiment.polarity)
-            recent_avg_sentiment_polarity = scores.mean()
-            recent_min_sentiment_polarity = scores.min()
-                                                              
-        test = [[no_follow, author_verified, author_comment_karma, author_link_karma, over_18, is_submitter, 
-                 recent_num_comments, recent_num_last_30_days, recent_avg_no_follow, recent_avg_gilded, 
-                 recent_avg_responses, recent_percent_neg_score, recent_avg_score, recent_min_score, 
-                 recent_avg_controversiality, recent_avg_ups, recent_avg_diff_ratio, recent_max_diff_ratio, 
-                 recent_avg_sentiment_polarity, recent_min_sentiment_polarity]]
-        print(test)
-        
-        prediction = model.predict(test)
+        prediction = model.predict(clean_data)
         print(prediction)
 
         # Return the prediction
--- model.py
+++ model.py
@@ -1,15 +1,21 @@
 """Build Randon Forest Model."""
-from sklearn.ensemble import RandomForestClassifier
-from datacleaner import autoclean
+from sklearn.tree import DecisionTreeClassifier
 import pickle
+import pandas as pd
+import collections
+import datetime as dt
+import numpy as np
+from textblob import TextBlob
+import json
+import difflib
 
 
 class RFModel(object):
     """Build Randon Forest Model for save this in .pkl file."""
     def __init__(self):
         """Define function for create .pkl file."""
-        self.clf = RandomForestClassifier()
-        self.clean = autoclean
+        #self.clf = DecisionTreeClassifier()
+        #self.clean
 
     def clean_data(self, my_data):
         """Clean a dataframe.
@@ -23,9 +29,87 @@ class RFModel(object):
         Clean data.
 
         """
-        return self.clean(my_data)
+        
+        no_follow = bool(my_data['no_follow'])
+        created_utc = int(my_data['created_utc'])
+        author_verified = bool(my_data['author_verified'])
+        author_comment_karma = float(my_data['author_comment_karma'])
+        author_link_karma = float(my_data['author_link_karma'])
+        over_18 = bool(my_data['over_18'])
+        is_submitter = bool(my_data['is_submitter'])
+        body = str(my_data['body'])
+        recent_comments = str(my_data['recent_comments'])
+        recent_num_comments = 0
+        recent_num_last_30_days = 0
+        recent_avg_no_follow = 0
+        recent_avg_gilded = 0
+        recent_avg_responses = 0
+        recent_percent_neg_score = 0
+        recent_avg_score = 0
+        recent_min_score = 0
+        recent_avg_controversiality = 0
+        recent_avg_ups = 0
+        recent_avg_diff_ratio = 0
+        recent_max_diff_ratio = 0
+        recent_avg_sentiment_polarity = 0
+        recent_min_sentiment_polarity = 0
+        
+        created_utc = pd.to_datetime(created_utc, unit='s')
+        
+        # remove escape characters to make parsing easier
+        recent_comments = recent_comments.replace('\\','');
+        
+        def diff_ratio(_a, _b):
+            return difflib.SequenceMatcher(a=_a,b=_b).ratio()
 
-    def create(self, n_estimators):
+        def last_30(a, b):
+            return a - dt.timedelta(days=30) < pd.to_datetime(b, unit='s')
+        
+        recent_comments = pd.read_json(recent_comments, dtype={
+            "banned_by": str,
+            "no_follow": bool,
+            "link_id": str,
+            "gilded": np.float64,
+            "author": str,
+            "author_verified": bool,
+            "author_comment_karma": np.float64,
+            "author_link_karma": np.float64,
+            "num_comments": np.float64,
+            "created_utc": np.float64,
+            "score": np.float64,
+            "over_18": bool,
+            "body": str,
+            "downs": np.float64,
+            "is_submitter": bool,
+            "num_reports": np.float64,
+            "controversiality": np.float64,
+            "quarantine": bool,
+            "ups": np.float64})
+        if(len(recent_comments) > 0):
+            recent_num_comments = len(recent_comments)
+            recent_num_last_30_days = recent_comments['created_utc'].apply(lambda x: last_30(created_utc, x)).sum()
+            recent_avg_no_follow = recent_comments['no_follow'].mean()
+            recent_avg_gilded = recent_comments['gilded'].mean()
+            recent_avg_responses = recent_comments['num_comments'].mean()
+            recent_percent_neg_score = recent_comments['score'].apply(lambda x: x < 0).mean() * 100
+            recent_avg_score = recent_comments['score'].mean()
+            recent_min_score = recent_comments['score'].min()
+            recent_avg_controversiality = recent_comments['controversiality'].mean()
+            recent_avg_ups = recent_comments['ups'].mean()
+            diff = recent_comments['body'].str.slice(stop=200).fillna('').apply(lambda x: diff_ratio(body, x))
+            recent_avg_diff_ratio = diff.mean()
+            recent_max_diff_ratio = diff.max()
+            scores = recent_comments['body'].append(pd.Series([body])).apply(lambda x: TextBlob(x).sentiment.polarity)
+            recent_avg_sentiment_polarity = scores.mean()
+            recent_min_sentiment_polarity = scores.min()
+            
+        return [[no_follow, author_verified, author_comment_karma, author_link_karma, over_18, is_submitter, 
+                 recent_num_comments, recent_num_last_30_days, recent_avg_no_follow, recent_avg_gilded, 
+                 recent_avg_responses, recent_percent_neg_score, recent_avg_score, recent_min_score, 
+                 recent_avg_controversiality, recent_avg_ups, recent_avg_diff_ratio, recent_max_diff_ratio, 
+                 recent_avg_sentiment_polarity, recent_min_sentiment_polarity]]
+
+    def create(self, max_depth):
         """Create a Random Forest Model.
 
         Parameters
@@ -37,7 +121,7 @@ class RFModel(object):
         Randon Forest model.
 
         """
-        self.clf = RandomForestClassifier(n_estimators, max_depth=2)
+        self.clf = DecisionTreeClassifier(max_depth=max_depth, class_weight={0:1,1:2.5,2:5}, min_samples_leaf=100)
 
     def train(self, X, y):
         """Train a model.
@@ -82,7 +166,7 @@ class RFModel(object):
         """
         return self.clf.feature_importances_
 
-    def pickle_clf(self, path='lib/models/RandomForestClassifier.pkl'):
+    def pickle_clf(self, path='lib/models/DecisionTreeClassifier.pkl'):
         """Saves the trained classifier for future use.
 
         Parameters
@@ -93,7 +177,7 @@ class RFModel(object):
         -------
         """
         with open(path, 'wb') as f:
-            pickle.dump(self.clf, f)
+            pickle.dump(self.train, f)
 
         print("Pickled classifier at {}".format(path))
 
@@ -108,5 +192,5 @@ class RFModel(object):
         -------
         """
         with open(path, 'wb') as f:
-            pickle.dump(self.clean, f)
+            pickle.dump(self.clean_data, f)
         print("Pickled vectorizer at {}".format(path))
